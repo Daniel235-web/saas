e092e105d53efb335e3e75821c3f9a17f1da1dbc
Daniel235-web <samueledaniel235@gmail.com>
2023-10-22 21:57:23 +0100

    finishing up the database sync and also debugging

--- src/app/_trpc/client.ts
+++ src/app/_trpc/client.ts
@@ -1,4 +1,4 @@
 import { AppRouter } from "@/trpc"
-import {createTRPCReact} from  "@trpc/react-query"
+import { createTRPCReact } from "@tRPC/react-query";
 
 export const  trpc = createTRPCReact<AppRouter>({})
