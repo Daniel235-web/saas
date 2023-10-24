e27a37b03e266092b07a15630d71e3c8643a7b6a
Daniel235-web <samueledaniel235@gmail.com>
2023-10-24 01:10:20 +0100

    fishing database sync

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,25 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
-import { redirect } from "next/navigation"
+import Dashboard from '@/components/Dashboard'
+import { db } from '@/db'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { redirect } from 'next/navigation'
 
+const Page = async () => {
+  const { getUser } = getKindeServerSession()
+  const user = getUser()
 
-const Page = () => {
-    const {getUser} = getKindeServerSession()
-    const user = getUser()
+  if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
 
-    if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
+  const dbUser = await db.user.findFirst({
+    where: {
+      id: user.id
+    }
+  })
 
-    return <div>{user.email}</div>
+  if(!dbUser) redirect('/auth-callback?origin=dashboard')
+
+  
+
+  return <Dashboard />
 }
 
 export default Page
---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,6 @@
+const Dashboard = () => {
+
+    return (<div>Hello world</div>)
+}
+
+export default Dashboard
