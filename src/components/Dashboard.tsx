ec1830ccb3daa9fbb7e76240879dcc093744b2d7
Daniel235-web <samueledaniel235@gmail.com>
2023-10-25 13:02:23 +0100

    let user delete files on dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -7,10 +7,28 @@ import Skeleton from "react-loading-skeleton";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { format } from "date-fns";
+import { useState } from "react";
 
 const Dashboard = () => {
+  const [currentlyDeletingFile, setCurrentlyDeletingFile] = useState<
+    string | null
+  >(null);
+
+  const utils = trpc.useContext();
+
   const { data: files, isLoading } = trpc.getUserFiles.useQuery();
-  const { mutate: deleteFile } = trpc.deleteFile.useMutation();
+
+  const { mutate: deleteFile } = trpc.deleteFile.useMutation({
+    onSuccess: () => {
+      utils.getUserFiles.invalidate();
+    },
+    onMutate({ id }) {
+      setCurrentlyDeletingFile(id);
+    },
+    onSettled() {
+      setCurrentlyDeletingFile(null);
+    },
+  });
   return (
     <main className="mx-auto max-w-7xl md:p-10">
       <div className="mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0">
